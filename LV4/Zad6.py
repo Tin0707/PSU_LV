c4c424e356106a3f432be971048c05cd87c2bdbd
Tin0707 <39195544+Tin0707@users.noreply.github.com>
2025-06-18 17:42:18 +0200

    Create Zad6.py

---
+++ LV4/Zad6.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error, max_error
+
+df = pd.read_csv('C:/Users/brnar/Downloads/cars_processed.csv')
+
+df = df.drop(columns=['name'])
+
+df_encoded = pd.get_dummies(df, drop_first=True)
+
+X = df_encoded.drop(columns=['selling_price'])
+y = df_encoded['selling_price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=300)
+
+scaler = StandardScaler()
+X_train_s = scaler.fit_transform(X_train)
+X_test_s = scaler.transform(X_test)
+
+linear_model = LinearRegression()
+linear_model.fit(X_train_s, y_train)
+
+y_pred_train = linear_model.predict(X_train_s)
+y_pred_test = linear_model.predict(X_test_s)
+
+print("R2 test", r2_score(y_test, y_pred_test))
+print("RMSE test:", np.sqrt(mean_squared_error(y_test, y_pred_test)))
+print("Max error test:", max_error(y_test, y_pred_test))
+print("MAE test:", mean_absolute_error(y_test, y_pred_test))
+
+fig = plt.figure(figsize=[13, 10])
+ax = sns.regplot(x=y_pred_test, y=y_test, line_kws={'color': 'green'})
+ax.set(xlabel='Predikcija', ylabel='Stvarna vrijednost', title='Rezultati na testnim podacima - s kategoričkim varijablama')
+plt.show()
